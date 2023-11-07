fd119fd1ba77b12b05ddecf1da15adbaf1f1bd45
yuto-masuda <yuto.masuda.0719@gmail.com>
2023-11-07 13:07:15 +0900

    位置情報取得、Map表示

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,38 +1,56 @@
-import React from "react";
-import { GoogleMap, LoadScript } from "@react-google-maps/api";
+import React, { useState, useEffect } from "react";
+import { GoogleMap, LoadScript, MarkerF } from "@react-google-maps/api";
 
-
-// マップ
-const containerStyle = {
-  width: "100%",
-  height: "86vh",
-};
-
-const center = {
-  lat: 35.55138821256619,
-  lng: 139.44547619305862,
-};
-
-const zoom = 13;
 
 const Map = () => {
+  // const [latitude, setLatitude] = useState('');
+  // const [longitude, setLongitude] = useState('');
+  const [position, setPosition] = useState({
+    lat: 0,
+    lng: 0,
+  });
+  const containerStyle = {
+    width: "100%",
+    height: "86vh",
+  };
+  const zoom = 13;
+
+
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition((position) => {
+      // setLatitude(position.coords.latitude)
+      // setLongitude(position.coords.longitude)
+      setPosition({...position, lat: position.coords.latitude, lng: position.coords.longitude})
+      console.log(position.coords)
+    },
+    (err) => {
+      console.log(err);
+    })
+
+  }, [])
+
   return (
-    <LoadScript googleMapsApiKey={process.env.GOOGLE_MAPS_API_KEY}>
-      <GoogleMap
-        mapContainerStyle={containerStyle}
-        center={center}
-        zoom={zoom}
-      ></GoogleMap>
-    </LoadScript>
-  );
-};
+    <>
+      <LoadScript googleMapsApiKey={process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY}>
+        <GoogleMap
+          mapContainerStyle={containerStyle}
+          center={position}
+          zoom={zoom}
+        >
+          <MarkerF position={position} label={"現在地"} />
+        </GoogleMap>
+      </LoadScript>
+    </>
+  )
+}
 
 
 export default function Home() {
-  console.log("test")
   return (
     <>
-      <Map />
+      <div>
+        <Map />
+      </div>
     </>
   )
 }
