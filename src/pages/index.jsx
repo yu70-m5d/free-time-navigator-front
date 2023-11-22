8b9221cb4e3eef2e62a3553a7f9fdfb183363fd8
yuto-masuda <yuto.masuda.0719@gmail.com>
2023-11-22 21:59:04 +0900

    位置情報半径1.5km以内のスポットを取得、表示

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,52 +1,78 @@
 import React, { useEffect, useState } from "react";
 import BasicCard from "../components/BasicCard";
 
-// RailsAPIとの通信
-export async function getStaticProps() {
-  const res = await fetch(`${process.env.NEXT_PUBLIC_FTN_API_INDEX}`);
-  const spots = await res.json();
 
-  return {
-    props: {
-      spots,
-    },
-    revalidate: 60 * 60 * 24,
-  };
-}
+export default function Home() {
+  // 状態管理
+  //// 現在地
+  const [origin, setOrigin] = useState({
+    lat: 0,
+    lng: 0,
+  });
 
+  //// 取得したspots
+  const [fetchedSpots, setFetchedSpots] = useState([]);
 
-export default function Home( {spots} ) {
+  //// useEffect完了の状態を管理
+  const [isEffectComplete, setIsEffectComplete] = useState(false);
 
-    // 現在地を取得する
-    const [origin, setOrigin] = useState({
-      lat: 0,
-      lng: 0,
-    });
+  // 初回マウント時に実行
+  useEffect(() => {
+    // Railsからspot情報を持ってくる
+    const sendOriginToRails = async (origin) => {
+      try {
+        const queryParams = new URLSearchParams({ lat: origin.lat, lng: origin.lng });
+        const url = `${process.env.NEXT_PUBLIC_FTN_API_INDEX}?${queryParams}`;
+        const res = await fetch(url);
+        const spots = await res.json();
 
-    // useEffect完了の状態を管理
-    const [isEffectComplete, setIsEffectComplete] = useState(false);
+        return spots
 
-    useEffect(() => {
-      navigator.geolocation.getCurrentPosition((position) => {
-        setOrigin({
-          ...origin,
-          lat: position.coords.latitude,
-          lng: position.coords.longitude
+      } catch (error) {
+        console.log("error", error);
+      }
+    };
+
+    // スポット情報を取得する
+    const getSpots = async () => {
+      try {
+        const position = await new Promise((resolve, reject) => {
+          navigator.geolocation.getCurrentPosition(resolve, reject);
         });
-        setIsEffectComplete(true);
-      },
-      (err) => {
-        console.log(err);
-        setIsEffectComplete(true);
-      })
-    }, []);
 
-    if (!isEffectComplete) return <div>現在地を取得しています。</div>;
+        // 現在地を更新
+        setOrigin({
+          lat: position.coords.latitude,
+          lng: position.coords.longitude,
+        })
+
+        // 現在地をRailsAPIに渡し、spotsを受け取る
+        const fetchedSpots = await sendOriginToRails({
+          lat: position.coords.latitude,
+          lng: position.coords.longitude,
+        });
+
+        // spots状態を更新
+        setFetchedSpots(fetchedSpots);
+
+        // useEffect状態を更新
+        setIsEffectComplete(true);
+      } catch (error) {
+        console.error("error", error);
+        setIsEffectComplete(true);
+      }
+    };
+
+    getSpots();
+  }, []);
+
+
+  if (!isEffectComplete) return <div>現在地を取得しています。</div>;
 
   return (
     <>
       <div>
-        {spots.map((spot) => (
+        {fetchedSpots.map((spot) => (
           <BasicCard key={spot.id} {...spot} origin={origin} />
         ))}
       </div>
