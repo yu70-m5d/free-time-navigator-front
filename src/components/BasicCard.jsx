a0826cc070b635a9581a98db95c873fe78a78409
yuto-masuda <yuto.masuda.0719@gmail.com>
2023-12-04 20:31:39 +0900

    滞在時間、住所表示

--- src/components/BasicCard.jsx
+++ src/components/BasicCard.jsx
@@ -18,28 +18,41 @@ const bull = (
 );
 
 export default function BasicCard(props) {
-  const { id, name, address, latitude, longitude, rating, image, duration, origin } = props;
+  const { id, name, address, latitude, longitude, rating, image, duration, origin, tags } = props;
+
   const router = useRouter()
   const queryParams = `origin=${encodeURIComponent(JSON.stringify(origin))}`;
   const isContactPage = router.pathname === "/spots/[id]"
 
-
   if (isContactPage) {
+    const regex = /[^0-9]/g;
+    const result = duration ? duration.replace(regex, "") : "";
+    const durationTime = parseInt(result);
+
+    const aryMax = (a, b) => {return Math.max(a, b);}
+    const aryMin = (a, b) => {return Math.min(a, b);}
+    const stayTimeAry = tags.map((tag) => tag.stay_time);
+    const max = stayTimeAry.reduce(aryMax);
+    const min = stayTimeAry.reduce(aryMin);
+    const ave = ( max + min ) / 2;
+
     return (
       <Card sx={{ width: 288 , display: 'flex', padding: '4px auto', margin: '4px auto'}}>
         <CardContent sx={{width: 266}}>
           <Typography variant="h5" component="div" sx={{fontSize: 16}}>
             {name}
             <br />
+            所要時間:{durationTime + ave}分
+            <br />
             移動時間:{duration}
+            <br />
+            { max === min ? `滞在時間:${ave}分` : `滞在時間:${min}分~${max}分` }
           </Typography>
           <Typography sx={{ mb: 1.5, fontSize: 14 }} color="text.secondary">
             {rating}
           </Typography>
           <Typography variant="body2">
-            well meaning and kindly.
-            <br />
-            {'"a benevolent smile"'}
+            {address}
           </Typography>
         </CardContent>
       </Card>
